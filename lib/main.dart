0d58f202f2b7a56383cfc7a3e96724d1ec52471c
Dustinouwu <brago22@hotmail.it>
2026-02-17 16:47:48 -0500

    Arreglo de proyecto

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:protolove_iritech/routes/routes.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:provider/provider.dart';
+
+import 'screens/screen.dart';
+import 'service/app_service.dart';
+import 'service/service.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'utils/utils.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AppService(PreferencesService())..init(),
+        ),
+        ChangeNotifierProvider(create: (_) => AuthService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+// Acceso global al cliente de Supabase
+final supabase = Supabase.instance.client;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Protolove',
+      navigatorKey: NavigationService().navigatorKey,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      routes: AppRoutes.getRoutes(),
+      initialRoute: SplashScreen.routeName,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
